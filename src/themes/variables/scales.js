13cd29fe39663fa2abd8c36a0b7d8d5f9ddf9a56
nishammahsin <nisham.mahsin@gmail.com>
2019-01-10 17:14:30 +0530

    1. add scales for button components

--- src/themes/variables/scales.js
+++ src/themes/variables/scales.js
@@ -1,8 +1,22 @@
 import { Platform, PixelRatio, getPixelSizeForLayoutSize } from "react-native";
 
+const platform = Platform.OS;
 
 export const defaultScales = {
- 
+  button: {
+    paddingDefaultOrSm:`${3 * PixelRatio.get()}px ${8 * PixelRatio.get()}px ${3 * PixelRatio.get()}px ${8 * PixelRatio.get()}px`,
+    paddingLg:`${5 * PixelRatio.get()}px ${16 * PixelRatio.get()}px ${5 * PixelRatio.get()}px ${16 * PixelRatio.get()}px`,
+    paddingSm:`${1.5 * PixelRatio.get()}px ${6 * PixelRatio.get()}px ${1.5 * PixelRatio.get()}px ${6 * PixelRatio.get()}px`,
+    borderRadius: "4px",
+    borderRadiusRounded: '50px',
+    borderRadiusFull: '0',
+    borderWidth: "1px",
+    minWidthDefault: `${PixelRatio.getPixelSizeForLayoutSize(40)}px`,
+    minWidthSm: `${30 * PixelRatio.get()}px`,
+    minWidthFull: '100%',
+    minWidthBlock: '100%',
+    disabledOpacity: '.65'
+  }
 };
 
 // more scales can be created here and pass it to the setThemeColors function
